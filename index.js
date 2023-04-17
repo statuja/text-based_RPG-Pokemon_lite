f3e37f25eac65d4889cbbcdca8036078342ae929
dci-student <irinasolovich@gmail.com>
2023-04-17 14:34:45 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,18 @@
+class Pokemon {
+    constructor(name, length, magic, skills) {
+        this.name = name;
+        this.magic = length;
+        this.magic = magic;
+        this.skills = skills
+    };
+}
+const skills = ["run", "sleep", "eat", "wash"];
+
+class AttackSkill {
+    constructor (name, damage, quantity) {
+        this.name = name;
+        this.damage = damage;
+        this.quantity = quantity
+    }
+} 
+let lightning = new AttackSkill("lightning", 40, 30);
